cd4a7fae80dff8d17440732b582eaba6a3f5dd2d
anoverose <aidanamamasharipova15@gmail.com>
2026-02-24 09:44:46 +0600

    Update Person.java

--- src/main/java/class5/Person.java
+++ src/main/java/class5/Person.java
@@ -3,17 +3,17 @@ public class Person {
         String name;
         String surname;
         int age;
-        int hight;
-        Person(String name,String surname,int age, int hight){
+        int height;
+        Person(String name,String surname,int age, int height){
             this.name = name;
             this.surname = surname;
             this.age =age;
-            this.hight = hight;
+            this.height = height;
         }
         void print(){
             System.out.println("Name: "+name);
             System.out.println("Surname: "+surname);
             System.out.println("Age: "+age);
-            System.out.println("Hight: "+hight);
+            System.out.println("Height: "+height);
         }
     }
